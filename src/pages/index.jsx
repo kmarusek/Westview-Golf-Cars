bafdbd46249ef3e1d29f4f8ee11e1e94d0db6f0c
nesidbod <nesidbod@gmail.com>
2020-05-15 15:19:02 +0300

    fix style 1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Button from '../components/Button';
 import Card from '../components/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "gatsby";
+import '../static/style/global.scss';
 
 
 const LandingPage = () => {
